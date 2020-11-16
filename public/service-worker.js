4ff95a564405016ca517d0a01ec1196094b6da9b
Aaron Ray <aaray619@gmail.com>
2020-11-16 15:12:45 -0600

    made changes to service worker to accomodate given code

--- public/service-worker.js
+++ public/service-worker.js
@@ -45,16 +45,16 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   // non GET requests are not cached and requests to other origins are not cached
-  if (
-    event.request.method !== "GET" ||
-    !event.request.url.startsWith(self.location.origin)
-  ) {
-    event.respondWith(fetch(event.request));
-    return;
-  }
+      // if (
+      //   event.request.method !== "GET" ||
+      //   !event.request.url.startsWith(self.location.origin)
+      // ) {
+      //   event.respondWith(fetch(event.request));
+      //   return;
+      // }
 
   // handle runtime GET requests for data from /api routes
-  if (event.request.url.includes("/api/images")) {
+  if (event.request.url.includes("/api/")) {
     // make network request and fallback to cache if network request fails (offline)
     event.respondWith(
       caches.open(RUNTIME_CACHE).then(cache => {
